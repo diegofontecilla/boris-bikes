2615b1d3f7a3a2dd0b7aef2641700afa9eaa1570
diego fontecilla <mail@diegofontecilla.com>
2018-05-16 19:06:00 +0100

    12 at some point

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -29,13 +29,12 @@ describe DockingStation do
 
     it 'returns a working bike' do
        bike = Bike.new
-       subject.release_bike
-       # DockingStation.new.release_bike
-      expect(bike).to be_working
+       subject.dock(bike)
+       expect(subject.release_bike).to be_working
     end
 
     it 'raises an error when there are no bikes available' do
-      expect { subject.release_bike }.to raise_error 'bugger'
+      expect { subject.release_bike }.to raise_error('No bikes available')
     end
 
   end
